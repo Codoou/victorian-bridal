e8b8ad06098ceea578031325cfe5638248d23f81
Cody Zimmerman <cody.w.zimmerman@gmail.com>
2020-01-04 20:56:42 -0600

    Initial commit

---
+++ Scripts/Home/home.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    // Homepage Slider
+    $(document).ready(function () {
+        $('.home-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000
+        });
+    });
+})();
